50d002064414435045f88ce3dabca4ee5e05f183
Moler <moler555@gmail.com>
2018-01-30 20:16:29 +0100

    Task2: Customer id changed to Integer

--- src/main/java/com/cinema/cinemaapp/repository/CustomerRepository.java
+++ src/main/java/com/cinema/cinemaapp/repository/CustomerRepository.java
@@ -2,8 +2,8 @@ package com.cinema.cinemaapp.repository;
 
 
 
-public interface CustomerRepository extends CrudRepository<Long, Customer> {
-    Customer deleteCustomerById(Long id);
+public interface CustomerRepository extends CrudRepository<Integer, Customer> {
+    Customer deleteCustomerById(Integer id);
     Customer findCustomerById(Integer id);
     Customer findCustomerByEmail(String email);
     List<Customer> findAll();
